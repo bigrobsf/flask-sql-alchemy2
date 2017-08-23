91a770a41dea3cfa65f9d8052138323bb701378a
Rob Conner <rjconner@mac.com>
2017-08-22 21:46:22 -0700

    prepare for heroku

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_modus import Modus
+from forms import UserForm, MessageForm
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask-sql-alchemy2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
@@ -56,15 +59,19 @@ def root():
 @app.route('/users', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        user_name = request.form.get('user_name')
-        first_name = request.form.get('first_name')
-        last_name = request.form.get('last_name')
-        email = request.form.get('email')
+        form = UserForm(request.form)
+        if form.validate():
+            user_name = request.form.get('user_name')
+            first_name = request.form.get('first_name')
+            last_name = request.form.get('last_name')
+            email = request.form.get('email')
 
-        db.session.add(User(user_name, first_name, last_name, email))
-        db.session.commit()
+            db.session.add(User(user_name, first_name, last_name, email))
+            db.session.commit()
 
-        return redirect(url_for('index'))
+            return redirect(url_for('index'))
+        else:
+            return render_template('users/new.html', form=form)
     return render_template('users/index.html', users=User.query.order_by(User.user_name).all())
 
 
@@ -72,15 +79,18 @@ def index():
 def show(id):
     found_user = User.query.get_or_404(id)
     if request.method == b'PATCH':
-        found_user.user_name = request.form.get('user_name')
-        found_user.first_name = request.form.get('first_name')
-        found_user.last_name = request.form.get('last_name')
-        found_user.email = request.form.get('email')
+        form = UserForm(request.form)
+        if form.validate():
+            found_user.user_name = request.form.get('user_name')
+            found_user.first_name = request.form.get('first_name')
+            found_user.last_name = request.form.get('last_name')
+            found_user.email = request.form.get('email')
 
-        db.session.add(found_user)
-        db.session.commit()
-        return redirect(url_for('index'))
-
+            db.session.add(found_user)
+            db.session.commit()
+            return redirect(url_for('index'))
+        else:
+            return render_template('users/edit.html', form=form)
     if request.method == b'DELETE':
         db.session.delete(found_user)
         db.session.commit()
@@ -93,12 +103,14 @@ def show(id):
 @app.route('/users/<int:id>/edit')
 def edit(id):
     found_user = User.query.get_or_404(id)
-    return render_template('users/edit.html', user=found_user)
+    form = UserForm(obj=found_user)
+    return render_template('users/edit.html', form=form, user=found_user)
 
 
 @app.route('/users/new')
 def new():
-    return render_template('users/new.html')
+    form = UserForm(request.form)
+    return render_template('users/new.html', form=form)
 
 # =============================================================================
 # routes for messages
@@ -106,18 +118,21 @@ def new():
 @app.route('/users/<int:user_id>/messages', methods=['GET', 'POST'])
 def message_index(user_id):
     if request.method == 'POST':
-        message = request.form.get('message')
+        form = MessageForm(request.form)
+        if form.validate():
+            message = request.form.get('message')
 
-        db.session.add(Message(message, user_id))
-        db.session.commit()
+            db.session.add(Message(message, user_id))
+            db.session.commit()
 
-        return redirect(url_for('message_index', user_id=user_id))
+            return redirect(url_for('message_index', user_id=user_id))
+        else:
+            return render_template('messages/new.html', form=form, user_id=user_id)
     else:  
         user = User.query.get(user_id)
         messages = user.messages.all()
-        info = [user, messages]
 
-        return render_template('messages/index.html', info=info)
+        return render_template('messages/index.html', user=user, messages=messages)
 
 
 @app.route('/users/<int:user_id>/messages/show/<int:msg_id>', methods=['GET', 'PATCH', 'DELETE'])
@@ -143,12 +158,14 @@ def message_show(user_id, msg_id):
 @app.route('/users/<int:user_id>/messages/<int:msg_id>/edit')
 def message_edit(user_id, msg_id):
     found_message = Message.query.get_or_404(msg_id)
-    return render_template('messages/edit.html', msg=found_message)
+    form = MessageForm(obj=found_message)
+    return render_template('messages/edit.html', form=form, msg=found_message)
 
 
 @app.route('/users/<int:user_id>/messages/new')
 def message_new(user_id):
-    return render_template('messages/new.html', user_id=user_id)
+    form = MessageForm(request.form)
+    return render_template('messages/new.html', form=form, user_id=user_id)
 
 
 @app.errorhandler(404)
@@ -156,5 +173,10 @@ def page_not_found(e):
     return render_template('404.html')
 
 
+if os.environ.get('ENV') == 'production':
+    app.config.from_object('config.ProductionConfig')
+else:
+    app.config.from_object('config.DevelopmentConfig')
+
 if __name__ == '__main__':
-    app.run(port=3001, debug=True)
+    app.run()
